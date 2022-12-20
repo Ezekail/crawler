2619253529fc9dee92871422d141d916ade9b420
wzk <wang357016129@qq.com>
2022-12-20 15:54:02 +0800

    print resp

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ezekail/crawler.git
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	url := "https://www.thepaper.cn/"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("fetch url failed,err:%v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%v", resp.StatusCode)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("read content failed:%v", err)
+		return
+	}
+	fmt.Println("body:", string(body))
+}
