cf273b6c71e4211bbe6910d992d5a365fb4ed258
wzk <wang357016129@qq.com>
2022-12-24 21:02:35 +0800

    接口封装

---
+++ collect/collect.go
@@ -0,0 +1,81 @@
+package collect
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type Fetcher interface {
+	Get(url string) ([]byte, error)
+}
+
+type BaseFetch struct {
+}
+
+// Get 获取网页的内容，检测网页的字符编码并将文本统一转换为 UTF-8 格式
+func (b *BaseFetch) Get(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%d\n", resp.StatusCode)
+		return nil, err
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterMinEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+type BrowserFetch struct {
+	Timeout time.Duration
+}
+
+// Get 模拟浏览器访问
+func (b BrowserFetch) Get(url string) ([]byte, error) {
+	// 创建一个 HTTP 客户端 http.Client
+	client := &http.Client{
+		Timeout: b.Timeout,
+	}
+	// 然后通过 http.NewRequest 创建一个请求
+	request, err := http.NewRequest("Get", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("get url failed,err:%v\n", err)
+	}
+	// 在请求中调用 req.Header.Set 设置 User-Agent 请求头
+	request.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36")
+	// 最后调用 client.Do 完成 HTTP 请求
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyReader := bufio.NewReader(response.Body)
+	e := DeterMinEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+// DeterMinEncoding 检测并返回当前 HTML 文本的编码格式
+func DeterMinEncoding(r *bufio.Reader) encoding.Encoding {
+	//如果返回的 HTML 文本小于 1024 字节，我们认为当前 HTML 文本有问题，直接返回默认的 UTF-8 编码就好了
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		fmt.Printf("fetch error:%v", err)
+		return unicode.UTF8
+	}
+	// 检测并返回对应 HTML 文本的编码
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
--- main.go
+++ main.go
@@ -1,48 +1,29 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
+	"github.com/Ezekail/crawler.git/collect"
 	"github.com/PuerkitoBio/goquery"
-	"github.com/antchfx/htmlquery"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/unicode"
-	"golang.org/x/text/transform"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"regexp"
 	"strings"
+	"time"
 )
 
-// 前解析好正则表达式内容
-var headerRe = regexp.MustCompile(`<div class="small_cardcontent__BTALp"[\s\S]*?<h2>([\s\S]*?)</h2>`)
-
 func main() {
-	url := "https://www.thepaper.cn/"
-	body, err := Fetch(url)
-	if err != nil {
-		log.Printf("read content failed,%v", err)
-	}
-	// 正则表达式提取
-	matches := headerRe.FindAllSubmatch(body, -1)
-	for _, match := range matches {
-		fmt.Println("fetch card news:", string(match[1]))
+	// url := "https://www.thepaper.cn/"
+	// 需设置携带User-Agent访问才有数据
+	url := "https://book.douban.com/subject/1007305/"
+	//var f collect.Fetcher = &collect.BaseFetch{}
+	var f collect.Fetcher = collect.BrowserFetch{
+		Timeout: 300 * time.Millisecond,
 	}
 
-	// xpath匹配提取
-	// 解析 HTML 文本
-	doc, err := htmlquery.Parse(bytes.NewReader(body))
+	body, err := f.Get(url)
 	if err != nil {
-		fmt.Printf("htmlquery Parse failed,err:%v", err)
+		fmt.Printf("read content failed,%v", err)
+		return
 	}
-	// 通过 XPath 语法查找符合条件的节点
-	nodes := htmlquery.Find(doc, `//div[@class="small_cardcontent__BTALp"]//h2	`)
-	for _, node := range nodes {
-		fmt.Println("fetch card ", node.FirstChild.Data)
-	}
+	fmt.Println(string(body))
 	// Css选择器匹配提取
 	// 加载HTML文档
 	cssDoc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
@@ -59,35 +40,4 @@ func main() {
 
 	exist := strings.Contains(string(body), "疫情")
 	fmt.Printf("是否存在疫情:%v\n", exist)
-}
-
-// Fetch 获取网页的内容，检测网页的字符编码并将文本统一转换为 UTF-8 格式
-func Fetch(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		log.Printf("Error status code:%v", resp.StatusCode)
-	}
-	bodyReader := bufio.NewReader(resp.Body)
-	e := DeterMinEncoding(bodyReader)
-	// transform.NewReader 用于将 HTML 文本从特定编码转换为 UTF-8 编码
-	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
-	return ioutil.ReadAll(utf8Reader)
-}
-
-// DeterMinEncoding 检测并返回当前 HTML 文本的编码格式
-func DeterMinEncoding(r *bufio.Reader) encoding.Encoding {
-	//如果返回的 HTML 文本小于 1024 字节，我们认为当前 HTML 文本有问题，直接返回默认的 UTF-8 编码就好了
-	bytes, err := r.Peek(1024)
-	if err != nil {
-		log.Printf("fetch error:%v", err)
-		return unicode.UTF8
-	}
-	// 检测并返回对应 HTML 文本的编码
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
 }
